ffdb14e9b0860d97089053ce03b0e6ee45a35fe0
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-08-11 21:41:44 +1000

    correct the unfortunate misunderstanding in flags

--- cmd/slackinvite/main.go
+++ cmd/slackinvite/main.go
@@ -22,7 +22,6 @@ const (
 type params struct {
 	Token     string
 	Cookie    string
-	Host      string
 	Addr      string
 	Port      string
 	RC        recaptcha.ReCaptcha
@@ -35,8 +34,8 @@ func init() {
 	flag.StringVar(&cmdline.FieldsCfg, "cfg", os.Getenv("CONFIG_FILE"), "Config file with template values")
 	flag.StringVar(&cmdline.Token, "t", os.Getenv("TOKEN"), "slack `token`")
 	flag.StringVar(&cmdline.Cookie, "c", os.Getenv("COOKIE"), "slack `cookie`")
-	flag.StringVar(&cmdline.Host, "port", osenv.Value("PORT", defPort), "`port` to listen to")
 	flag.StringVar(&cmdline.Addr, "addr", os.Getenv("ADDR"), "host `address` for the listener, i.e. 127.0.0.1, if empty\nwill listen on all interfaces.")
+	flag.StringVar(&cmdline.Port, "port", osenv.Value("PORT", defPort), "`port` to listen to")
 	flag.StringVar(&cmdline.RC.SiteKey, "site-key", os.Getenv("RECAPTCHA_KEY"), "recaptcha `key`")
 	flag.StringVar(&cmdline.RC.SecretKey, "site-secret", os.Getenv("RECAPTCHA_SECRET"), "recaptcha `secret`")
 }
@@ -52,8 +51,6 @@ func main() {
 		cmdline.Port = defPort
 	}
 
-	dlog.Printf("listening on %s", cmdline.Addr)
-
 	client := slack.New(cmdline.Token, slack.OptionCookie("d", cmdline.Cookie))
 
 	fields, err := si.LoadFields(cmdline.FieldsCfg)
@@ -61,7 +58,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	si, err := si.New(cmdline.Addr+":"+cmdline.Port, nil, client, cmdline.RC, fields)
+	listenerAddr := cmdline.Addr + ":" + cmdline.Port
+	dlog.Printf("listening on %s", listenerAddr)
+	si, err := si.New(listenerAddr, nil, client, cmdline.RC, fields)
 	if err != nil {
 		dlog.Fatal(err)
 	}
